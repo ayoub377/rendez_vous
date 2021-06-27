4748a6dac107c0e922aa15971ed093a82e277e0d
Permouda <82866195+Permouda@users.noreply.github.com>
2021-06-27 19:17:50 +0100

    1.9

--- src/Composants/Home.js
+++ src/Composants/Home.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import Navbar1 from "./Navbar";
+import "./styles_css/per_styles.css"
+
 class Home extends Component {
     render() {
         return (
             <div>
                 <Navbar1/>
-                <h1 style={{textAlign:'center'}}>Prise de Rendez-vous</h1>
-
+<div className="justifyy">
+                <div className="titre">
+                <h2 data-text="Bienvenu_">Bienvenu_</h2>
+</div></div>
             </div>
         );
     }
